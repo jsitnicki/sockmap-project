37f87940b6830509ec2f752fb410f9c0ec1b08d4
Jakub Sitnicki <jakub@cloudflare.com>
2024-08-23 20:07:46 +0200

    Turn TCP proxy stub into a full proxy

--- examples/ingress-to-egress/tcp4-proxy.py
+++ examples/ingress-to-egress/tcp4-proxy.py
@@ -1,29 +1,69 @@
 #!/usr/bin/env python3
+#
+# TODO:
+# - accept alos second and subsequent connection
+#
 
-from signal import pause
-from socket import *
+"""\
+Usage: proxy.py <LISTEN-HOST> <LISTEN-PORT> <CONNECT-HOST> <CONNECT-PORT>
+"""
+
+import asyncio
 import sys
+from socket import *
 
-args = sys.argv[1:]
 
-if len(args) != 4:
-    print("Usage: proxy.py <LISTEN-HOST> <LISTEN-PORT> <CONNECT-HOST> <CONNECT-PORT>")
-    sys.exit(1)
+BUF_SIZE = 64 * 1024
 
-listen_host, listen_port, connect_host, connect_port = args
-listen_addr = (listen_host, int(listen_port))
-connect_addr = (connect_host, int(connect_port))
 
-ln = socket(AF_INET, SOCK_STREAM)
-ln.bind(listen_addr)
-ln.listen(1)
+async def proxy(source, target):
+    loop = asyncio.get_event_loop()
 
-p, _ = ln.accept()
-print(f"Accepted {p.getsockname()} → {p.getpeername()}")
+    print(f"proxy {source.getsockname()} → {target.getsockname()}")
+    while True:
+        buf = await loop.sock_recv(source, BUF_SIZE)
+        await loop.sock_sendall(target, buf)
 
-c = socket(AF_INET, SOCK_STREAM)
-c.connect(connect_addr)
-print(f"Connected {c.getsockname()} → {c.getpeername()}")
 
-print("Waiting for signal...")
-pause()
+async def run(listen_addr, connect_addr):
+    loop = asyncio.get_event_loop()
+
+    ln = socket(AF_INET, SOCK_STREAM)
+    ln.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    ln.bind(listen_addr)
+    ln.listen(SOMAXCONN)  # can't await for it with asyncio?
+    ln.setblocking(False)
+
+    tasks = set()
+    while True:
+        print("waiting for conn...")
+
+        p, _ = await loop.sock_accept(ln)
+        print(f"incoming conn {p.getpeername()} → {p.getsockname()}")
+
+        c = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK)
+        await loop.sock_connect(c, connect_addr)
+        print(f"outgoing conn {c.getsockname()} → {c.getpeername()}")
+
+        t1 = loop.create_task(proxy(p, c))
+        t2 = loop.create_task(proxy(c, p))
+
+        # keep tasks alive
+        tasks |= {t1, t2}
+
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 4:
+        print(__doc__, end="", file=sys.stderr)
+        sys.exit(1)
+
+    listen_host, listen_port, connect_host, connect_port = args
+    listen_addr = (listen_host, int(listen_port))
+    connect_addr = (connect_host, int(connect_port))
+
+    asyncio.run(run(listen_addr, connect_addr))
+
+
+main()
