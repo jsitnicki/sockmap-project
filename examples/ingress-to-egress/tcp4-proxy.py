8d2214efdfb426cb74367d5b1de0f079681e8141
Jakub Sitnicki <jakub@cloudflare.com>
2024-08-26 15:53:47 +0200

    tcp4-proxy.py: Accept multiple connections

--- examples/ingress-to-egress/tcp4-proxy.py
+++ examples/ingress-to-egress/tcp4-proxy.py
@@ -1,69 +1,104 @@
 #!/usr/bin/env python3
 #
-# TODO:
-# - accept alos second and subsequent connection
+# Single-threaded asynchronous TCP4 proxy
 #
 
-"""\
-Usage: proxy.py <LISTEN-HOST> <LISTEN-PORT> <CONNECT-HOST> <CONNECT-PORT>
-"""
-
-import asyncio
-import sys
+import asyncio, errno, logging, sys
 from socket import *
 
 
-BUF_SIZE = 64 * 1024
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
+info = logging.info
+warn = logging.warning
 
 
-async def proxy(source, target):
-    loop = asyncio.get_event_loop()
+async def proxy_stream(id, source, target):
+    BUF_SIZE = 64 * 1024
 
-    print(f"proxy {source.getsockname()} → {target.getsockname()}")
-    while True:
-        buf = await loop.sock_recv(source, BUF_SIZE)
-        await loop.sock_sendall(target, buf)
+    try:
+        info(f"proxy stream {id}")
+
+        loop = asyncio.get_running_loop()
+        while True:
+            buf = await loop.sock_recv(source, BUF_SIZE)
+            if not buf:
+                break
+            await loop.sock_sendall(target, buf)
+
+        source.shutdown(SHUT_RDWR)
+        target.shutdown(SHUT_RDWR)
+
+    except OSError as e:
+        if (
+            e.errno == errno.ENOTCONN  # other coro called shutdown
+            or e.errno == errno.ECONNRESET  # peer gone
+        ):
+            pass
+        else:
+            raise e
+
+    finally:
+        source.close()
+        target.close()
+        info(f"close stream {id}")
 
 
 async def run(listen_addr, connect_addr):
-    loop = asyncio.get_event_loop()
-
-    ln = socket(AF_INET, SOCK_STREAM)
+    ln = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK)
     ln.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     ln.bind(listen_addr)
-    ln.listen(SOMAXCONN)  # can't await for it with asyncio?
-    ln.setblocking(False)
+    ln.listen(SOMAXCONN)
 
     tasks = set()
+    loop = asyncio.get_running_loop()
     while True:
-        print("waiting for conn...")
+        info(f"waiting for conn at {ln.getsockname()}...")
 
         p, _ = await loop.sock_accept(ln)
-        print(f"incoming conn {p.getpeername()} → {p.getsockname()}")
+        info(f"incoming conn {p.getpeername()} ↱ {p.getsockname()}")
 
-        c = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK)
-        await loop.sock_connect(c, connect_addr)
-        print(f"outgoing conn {c.getsockname()} → {c.getpeername()}")
+        try:
+            c = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK)
+            await loop.sock_connect(c, connect_addr)
+            info(f"outgoing conn {c.getsockname()} ↳ {c.getpeername()}")
 
-        t1 = loop.create_task(proxy(p, c))
-        t2 = loop.create_task(proxy(c, p))
+            id1 = f"{p.getpeername()} ↱ {p.getsockname()} ↷ {c.getsockname()} ↳ {c.getpeername()}"
+            id2 = f"{c.getpeername()} ↱ {c.getsockname()} ↷ {p.getsockname()} ↳ {p.getpeername()}"
 
-        # keep tasks alive
-        tasks |= {t1, t2}
+            t1 = loop.create_task(proxy_stream(id1, p.dup(), c.dup()))
+            t2 = loop.create_task(proxy_stream(id2, c.dup(), p.dup()))
 
+            # TODO: clean up finished tasks
+            tasks |= {t1, t2}  # keep tasks alive
 
-def main():
-    args = sys.argv[1:]
+        except ConnectionRefusedError:
+            warn(f"failed to connect to {connect_addr}")
 
-    if len(args) != 4:
-        print(__doc__, end="", file=sys.stderr)
-        sys.exit(1)
-
-    listen_host, listen_port, connect_host, connect_port = args
-    listen_addr = (listen_host, int(listen_port))
-    connect_addr = (connect_host, int(connect_port))
-
-    asyncio.run(run(listen_addr, connect_addr))
+        finally:
+            p.close()
+            c.close()
 
 
-main()
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    p = ArgumentParser()
+    p.add_argument("listen_host")
+    p.add_argument("listen_port", type=int)
+    p.add_argument("connect_host")
+    p.add_argument("connect_port", type=int)
+
+    args = p.parse_args()
+    listen_addr = (args.listen_host, args.listen_port)
+    connect_addr = (args.connect_host, args.connect_port)
+
+    try:
+        asyncio.run(run(listen_addr, connect_addr))
+    except KeyboardInterrupt:
+        info("exit on signal...")
